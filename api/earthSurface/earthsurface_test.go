7622885f0a14c1c093265022a2c201114350de0b
Administrator <Administrator@BOSS>
2024-06-07 16:05:59 +0800

    1
    1

---
+++ api/earthSurface/earthsurface_test.go
@@ -0,0 +1,142 @@
+package earthSurface_test
+
+import (
+	"XrayR/api"
+	"XrayR/api/earthSurface"
+	"fmt"
+	"testing"
+)
+
+func CreateClient() api.API {
+	apiConfig := &api.Config{
+		APIHost:  "http://localhost:5005",
+		Key:      "qwertyuiopasdfghjkl",
+		NodeID:   3,
+		NodeType: "V2ray",
+	}
+	client := earthSurface.New(apiConfig)
+	return client
+}
+
+func TestGetV2rayNodeInfo(t *testing.T) {
+	client := CreateClient()
+	nodeInfo, err := client.GetNodeInfo()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(nodeInfo)
+}
+
+func TestGetSSNodeInfo(t *testing.T) {
+	apiConfig := &api.Config{
+		APIHost:  "http://localhost:5005",
+		Key:      "qwertyuiopasdfghjkl",
+		NodeID:   1,
+		NodeType: "Shadowsocks",
+	}
+	client := earthSurface.New(apiConfig)
+	nodeInfo, err := client.GetNodeInfo()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(nodeInfo)
+}
+
+func TestGetTrojanNodeInfo(t *testing.T) {
+	apiConfig := &api.Config{
+		APIHost:  "http://localhost:5005",
+		Key:      "qwertyuiopasdfghjkl",
+		NodeID:   1,
+		NodeType: "Trojan",
+	}
+	client := earthSurface.New(apiConfig)
+	nodeInfo, err := client.GetNodeInfo()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(nodeInfo)
+}
+
+func TestReportReportNodeOnlineUsers(t *testing.T) {
+	apiConfig := &api.Config{
+		APIHost:  "http://localhost:5005",
+		Key:      "qwertyuiopasdfghjkl",
+		NodeID:   3,
+		NodeType: "V2ray",
+	}
+	client := earthSurface.New(apiConfig)
+	userList, err := client.GetUserList()
+	if err != nil {
+		t.Error(err)
+	}
+
+	onlineUserList := make([]api.OnlineUser, len(*userList))
+	for i, userInfo := range *userList {
+		onlineUserList[i] = api.OnlineUser{
+			UID: userInfo.UID,
+			IP:  fmt.Sprintf("1.1.1.%d", i),
+		}
+	}
+	// client.Debug()
+	err = client.ReportNodeOnlineUsers(&onlineUserList)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGetUserList(t *testing.T) {
+	client := CreateClient()
+
+	userList, err := client.GetUserList()
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(userList)
+}
+
+func TestReportReportUserTraffic(t *testing.T) {
+	client := CreateClient()
+	userList, err := client.GetUserList()
+	if err != nil {
+		t.Error(err)
+	}
+	generalUserTraffic := make([]api.UserTraffic, len(*userList))
+
+	for i, userInfo := range *userList {
+		generalUserTraffic[i] = api.UserTraffic{
+			UID:      userInfo.UID,
+			Upload:   114514,
+			Download: 114514,
+		}
+	}
+	// client.Debug()
+	err = client.ReportUserTraffic(&generalUserTraffic)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(generalUserTraffic)
+}
+
+func TestGetNodeRule(t *testing.T) {
+	client := CreateClient()
+	client.Debug()
+	ruleList, err := client.GetNodeRule()
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(ruleList)
+}
+
+func TestReportNodeStatus(t *testing.T) {
+	client := CreateClient()
+	nodeStatus := &api.NodeStatus{
+		CPU: 1, Mem: 1, Disk: 1, Uptime: 256,
+	}
+	err := client.ReportNodeStatus(nodeStatus)
+	if err != nil {
+		t.Error(err)
+	}
+}
---
+++ api/earthSurface/model.go
@@ -0,0 +1,77 @@
+package earthSurface
+
+import (
+	"encoding/json"
+)
+
+type serverConfig struct {
+	RCode      int    `json:"code"`
+	RType      string `json:"type"`
+	RMessage   string `json:"message"`
+	ResultData struct {
+		NodeId      int     `json:"nodeid"`
+		NodeType    string  `json:"nodetype"`
+		ServerPort  int     `json:"port"`
+		SpeedLimit  float64 `json:"speedlimit"`
+		ClientLimit int     `json:"clientlimit"`
+		NodeSetting struct {
+			Cipher      string           `json:"cipher"`
+			Obfs        string           `json:"obfs"`
+			Path        string           `json:"path"`
+			ServerKey   string           `json:"serverkey"`
+			Network     string           `json:"network"`
+			Headers     *json.RawMessage `json:"headers"`
+			ServiceName string           `json:"serviceName"`
+			Header      *json.RawMessage `json:"header"`
+			Tls         int              `json:"tls"`
+			Host        string           `json:"host"`
+			Method      string           `json:"method"`
+			Sni         string           `json:"sni"`
+			PrivateKey  string           `json:"privateKey"`
+			ShortId     string           `json:"short_id"`
+			Dest        string           `json:"dest"`
+			xVer        uint64           `json:"xver"`
+			VlessFlow   string           `json:"flow"`
+		} `json:"setting"`
+		Routes []route `json:"routes"`
+	} `json:"result"`
+	RExtras string `json:"extras"`
+	RTime   string `json:"time"`
+}
+
+type route struct {
+	Id          int      `json:"id"`
+	Match       []string `json:"match"`
+	Action      string   `json:"action"`
+	ActionValue string   `json:"action_value"`
+}
+
+type user struct {
+	Id          int    `json:"id"`
+	PassWord    string `json:"password"`
+	SpeedLimit  uint64 `json:"nodespeedlimit"`
+	DeviceLimit int    `json:"nodeconnector"`
+}
+
+type OnlineUser struct {
+	UID int    `json:"UserNo"`
+	IP  string `json:"IP"`
+}
+type PostData struct {
+	Users   interface{} `json:"users"`
+	Onlines interface{} `json:"onlines"`
+}
+
+type UserTraffic struct {
+	UID      int   `json:"userno"`
+	Upload   int64 `json:"upload"`
+	Download int64 `json:"download"`
+}
+
+type NodeStatus struct {
+	CPU    string `json:"cpu"`
+	Memory string `json:"memory"`
+	Net    string `json:"net"`
+	Disk   string `json:"disk"`
+	Uptime int    `json:"uptime"`
+}
